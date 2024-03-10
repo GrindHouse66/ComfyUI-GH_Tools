f9ddf0e046008ab72caab819a3e1f0406ec85e87
GrindHouse66 <115404193+GrindHouse66@users.noreply.github.com>
2024-03-11 00:19:02 +1100

    images are fubar . . . .

--- GH_Tools.py
+++ GH_Tools.py
@@ -154,18 +154,16 @@ class GHImg_Vwr:
             print(f"Saved {file}")
         return { "ui": { "images": results } } 
     
-    def preview_images(self):
+    def preview_images(self, images):
         self.output_dir = folder_paths.get_temp_directory()
         self.type = "temp"
         self.prefix_append = "_temp_" + ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(5))
         self.compress_level = 1
 
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
-                    {"images": ("IMAGE", ), },
-                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
-                }
+        print(f"Saved {file}")
+        return { "ui": { "images": images } }
+
+   
 
     
 NODE_CLASS_MAPPINGS = {
